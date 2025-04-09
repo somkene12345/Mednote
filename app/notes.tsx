126903c541c3c03ee72a6d027309f3e01bd2c69a
somkene12345 <153438701+somkene12345@users.noreply.github.com>
2025-04-09 17:53:02 +0100

    Update notes.tsx

--- app/notes.tsx
+++ app/notes.tsx
@@ -327,8 +327,8 @@ export default function SearchPatientNotes() {
                             Test End: {formatDate(endTime)}
                           </Text>
                           <Text style={styles.sectionHeaderText}>
-                            Type: {item.TestType ? item.TestType : "Holter"}
-                          </Text>
+                            Type: {item.data?.[0]?.TestType || "Holter"}
+                         </Text>
                           <Text style={styles.sectionHeaderText}>
                           Raw Type: {JSON.stringify(item)}
                           </Text>
